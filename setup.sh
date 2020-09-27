daf10a392e9a9dfd74fdcb4bfe6545bc65c7066d
Apurva-tech <apurva.sharma866@gmail.com>
2020-09-27 18:18:10 +0530

    Setup update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"your@domain.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "
+[server]\n
+headless = true\n
+enableCORS=false\n
+port = $PORT\n
+\n
+" > ~/.streamlit/config.toml
